e3a7c1243fff5a0158d82104aa6febef500de19e
winsoft666 <winsoft666@outlook.com>
2024-04-17 21:21:43 +0800

    Update

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fbb46283a7b4d42d425356f8779708d5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
